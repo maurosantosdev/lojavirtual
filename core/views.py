bbe64e6137b6f1aaa65ab81d76e44c2e93c34fc8
Mauro Santos <mauro@bsd.com.br>
2018-07-06 20:23:22 -0300

    Projeto Mauro Santos

---
+++ core/views.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
