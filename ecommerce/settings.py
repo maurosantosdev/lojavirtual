2cc6ef315b94b4ed90f705436d9b08a9c1bf76a1
Mauro Santos <mauro@bsd.com.br>
2018-07-08 10:36:22 -0300

    Formulario Email 004

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # libs
-    'widget_tweaks',
+
     # apps
     'core',
     'catalog',
