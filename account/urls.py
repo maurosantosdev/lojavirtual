ffb26f96942c8cfb907419a37e742756927a5807
Mauro Santos <mauro@bsd.com.br>
2018-07-11 21:21:20 -0300

    Ajustes na conta

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('alterar-dados/', views.update_user, name='update_user'),
+    path('alterar-senha/', views.update_password, name='update_password'),
     path('registro/', views.register, name='register'),
 ]
