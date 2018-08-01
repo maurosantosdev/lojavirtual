b983b02bdc656e480b4acaacb2dbd71c931d140d
Mauro Santos <mauro@bsd.com.br>
2018-08-01 13:30:23 -0300

    E-commerce Mauro Santos

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     'checkout.middleware.cart_item_middleware',
 ]
 
