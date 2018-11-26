de2b12e2c269051153d57b485229547496339099
Mauro Santos <mauro@bsd.com.br>
2018-11-26 14:05:01 -0300

    Ecommerce - Mauro Santos 001

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ecommerce',
-        'USER': 'maurosantos',
-        'PASSWORD': '12345678',
+        'USER': 'name',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
