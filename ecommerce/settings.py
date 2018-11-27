ec6c588edb9b100ac54a8881833555bf69c6b2cf
Mauro Santos <mauro@bsd.com.br>
2018-11-27 09:42:56 -0300

    Ajuste no Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -208,4 +208,4 @@ except ImportError:
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
