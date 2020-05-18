7cfc83a30510bca4514928fbad64e0ea5703754b
Mauro Santos <mauro@bsd.com.br>
2020-05-17 20:41:08 -0400

    Atualizacao 07

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('alterar-dados/', views.update_user, name='update_user'),
     path('alterar-senha/', views.update_password, name='update_password'),
     path('registro/', views.register, name='register'),
-    path('address/', views.update_address, name='address')
+    path('address/', views.update_address, name='update_user')
 ]
